70806c97604f4960d996269c1d69bfcbeac3cd4c
ingridlarssen <ingrid_al_96@hotmail.com>
2018-04-26 15:11:30 +0200

    fetches data from random page

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import NavbarComponent from './components/Navbar';
+import ResultSubjects from './components/ResultSubjects';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
           <NavbarComponent />
+          <ResultSubjects />
       </div>
     );
   }
